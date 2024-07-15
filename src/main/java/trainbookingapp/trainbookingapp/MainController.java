8ff353188e5b57a28bf1cdacad8ea8bdfdc77830
dusklight00 <r.rahul.developer@gmail.com>
2024-07-15 21:35:45 +0530

    springboot setup complete

---
+++ src/main/java/trainbookingapp/trainbookingapp/MainController.java
@@ -0,0 +1,13 @@
+package trainbookingapp.trainbookingapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+  @RequestMapping("/home")
+  public String home() {
+    return "main-page.html";
+  }
+}
---
+++ src/main/java/trainbookingapp/trainbookingapp/TrainbookingappApplication.java
@@ -0,0 +1,13 @@
+package trainbookingapp.trainbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainbookingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainbookingappApplication.class, args);
+	}
+
+}
